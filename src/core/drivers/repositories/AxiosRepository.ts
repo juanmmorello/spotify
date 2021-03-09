5d66e24d3f5a59799d3141266a2cc41716561127
Juan Morello <juanmariamorello@gmail.com>
2021-03-08 21:11:19 -0300

    removed oauth token

--- src/core/drivers/repositories/AxiosRepository.ts
+++ src/core/drivers/repositories/AxiosRepository.ts
@@ -1,7 +1,7 @@
 import axios, {AxiosInstance} from "axios";
 
 export class AxiosRepository {
-  oAuthToken: String = 'BQCm1naeLdYlVnPxG6X43p3mY16QfL1HLu891R9NXsm_ktrDOUvSKAit9qLCSkmDY97wWtUwJWHwnNYDoIhOIE4QuYW2LCwmBXAqEtBwUOTGuTUli-79YvgE80O_B6MaU1C9yFgkmte4etPWdK4m-475KWH8LW32W_Ni4ivMYhqGQkd2LsmH6RckkCGo-1HUO-vZZdWF-5tJcm6w'
+  oAuthToken: String = ''
 
   client(): AxiosInstance {
     console.log(this.oAuthToken)
